0fab001183b7b13bc1eedd643502da48eb2a1e7f
Thomas <thomas.galocha@hotmail.fr>
2025-03-23 23:25:11 +0100

    feat : update back btn

--- src/components/utils/PreviousButton.vue
+++ src/components/utils/PreviousButton.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="btn btn-circle fixed bottom-10 right-5 btn-soft btn-primary btn-lg z-50">
+    <button
+        class="btn btn-circle fixed bottom-10 right-5 btn-soft btn-primary btn-lg z-50 md:top-90 md:left-150 md:btn-xl ">
         <slot>
             <i class="fas fa-arrow-left"></i>
         </slot>
